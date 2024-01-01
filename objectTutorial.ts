09c698dc2ae7f2f643bf916a598a9a3c9b4ca23e
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-02 01:01:29 +0530

    type aliases usecase

--- objectTutorial.ts
+++ objectTutorial.ts
@@ -20,3 +20,36 @@ function createCourse():{name: string, price: number}{
 //it is possible
 let newUser = {name: "Abhishek",age: 10, isActive: true, email: "Abhishek@gmail.com"};
 createUser(newUser); //This is not complain although email is not defined in the parameters
+
+
+//************* Type Aliases *****************/
+//we can use "type" to create our own datatype like classes in java or c++
+
+type Student = {
+    name : string;
+    age : number;
+    isActive : boolean;
+}
+
+//function which will accept only Student type of data
+function createStudent(student: Student){};
+
+//we can also make aliases of default types like number, string
+type Mystring = string;
+type Mynumber = number;
+
+// returning the Student type data from a function
+function newStudent(student: Student):Student{
+    // return student;
+    return {name: "abhi",age : 10,isActive : true,} //returning the Student type data
+}
+
+//creating a s1 student of Student type
+const  s1 : Student = {
+    name: "abhi",
+    age : 10,
+    isActive : true,
+}
+
+//calling the function with  passing s1 
+newStudent(s1);
