261c0836a6bc3dcf768b315532ddff344c62d718
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-01 23:23:33 +0530

    basic type of typescript

---
+++ variableMe.ts
@@ -0,0 +1,23 @@
+let greetings: string = "Hi abhishek";
+
+console.log(greetings);
+
+//number 
+let userId: number = 2341;
+
+userId.toFixed();
+
+//boolean
+let loggedIn: boolean = false;
+
+//type inference 
+let rollNumber = 10
+rollNumber.toFixed();
+
+//any
+let hero;
+function getHero(){
+    return 'tony';
+}
+
+hero = getHero();
